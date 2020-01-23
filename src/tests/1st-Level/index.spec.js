285e54e1e73cdc55f738d89200eb5d8831db409a
zsmith <zsmith-contractor@cars.com>
2020-01-23 14:44:18 -0500

    added new files + use strict

--- src/tests/1st-Level/index.spec.js
+++ src/tests/1st-Level/index.spec.js
@@ -1,1 +1,3 @@
+'use strict';
+
 // some unit tests for those code tests go here
---
+++ src/tests/2nd-Level/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// imports go here
+
+/*
+    This function is passed an array of arrays of varying depth. De-nest
+    all of the nested arrays and return a single, 1d array
+    with all of the values of each array, in order.
+
+    Return the new array.
+*/
+
+function assignmentOne(myArray) {
+
+    return null;
+}
+
+/*
+    This function is passed an array of arrays of varying depth. De-nest
+    ONLY up through the 2nd level. 3rd level arrays should not
+    be de-nested.
+
+    ex.
+    let arr = [
+        1 <- depth 0
+        [2, 3] <- depth 1
+        [4, [5]] <- the "5" is at depth 2
+    ];
+*/
+
+function assignmentTwo(myArray) {
+
+    return null;
+}
