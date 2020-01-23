9a1dfdde026d3aa89b127a7c67df083eb93beace
zsmith <zsmith-contractor@cars.com>
2020-01-23 11:19:40 -0500

    added some entry level tests

--- src/tests/1st-Level/index.js
+++ src/tests/1st-Level/index.js
@@ -1,1 +1,70 @@
+'use strict';
+
 // some code tests go here
+// imports go here
+/*
+    This function takes in an array. Using a higher-order Array function,
+    add a string of "foo" on to the end of every string in the array.
+
+    Return the array.
+*/
+function assignmentOne(myArray) {
+
+    return null;
+}
+
+/*
+    This function takes in an array. Using a higher-order Array function,
+    depending on the contents of the current string in the array:
+        - prepend the index of the current element to the element
+        - add on "foo" to the beginning of the string if it contains "bar"
+
+    Return the array.
+*/
+
+function assignmentTwo(myArray) {
+
+    return null;
+}
+
+/*
+    This function takes in an array. Using a higher order Array function,
+    reduce this array down to only elements that are of type string.
+
+    Return the array.
+*/
+
+function assignmentThree(myArray) {
+    
+    return null;
+}
+
+/*
+    This function takes in an array of chars. Using a higher order Array function,
+    turn this array into a string that consists of each element of the array, in order.
+
+    Input: [ 'G', 'i', 'r', 'a', 'f', 'f', 'e' ] // Output: 'Giraffe'
+*/
+
+function assignmentFour(myArray) {
+
+    return null;
+}
+
+/*
+    This function takes in an array of numbers. Using a higher order Array function,
+    add up each number in the array that happnens to be even AND is higher than 100.
+
+    Return the new total.
+*/
+
+function assignmentFive(myArray) {
+
+    return null;
+}
+
+/////////
+
+    // More entry level assignments here, focusing on Object {} manipulation.
+
+/////////
